c38496ea95c28cf8018e6e7db16d79caf592334a
lisskor <elizaveta.korotkova@gmail.com>
2024-01-08 15:25:14 +0100

    Build input examples for episodes at init

--- few_nerd_prompting/read_few_nerd.py
+++ few_nerd_prompting/read_few_nerd.py
@@ -14,16 +14,15 @@ class FewNerdEpisode:
         self.query_tokens = self.query_set['word']
         self.query_labels = self.query_set['label']
 
-        self.support_input_examples, self.support_output_examples = None, None
-        self.query_input_examples, self.query_output_examples = None, None
+        self.support_input_examples = [' '.join(sentence) for sentence in self.support_set['word']]
+        self.query_input_examples = [' '.join(sentence) for sentence in self.query_set['word']]
+        self.support_output_examples, self.query_output_examples = None, None
 
     def gpt_ner_examples_from_episode(self, entity_class: str):
-        self.support_input_examples = [' '.join(sentence) for sentence in self.support_set['word']]
         self.support_output_examples = [make_output_example(sentence, labels, entity_class)
                                         for sentence, labels in
                                         zip(self.support_set['word'], self.support_set['label'])]
 
-        self.query_input_examples = [' '.join(sentence) for sentence in self.query_set['word']]
         self.query_output_examples = [make_output_example(sentence, labels, entity_class)
                                       for sentence, labels in zip(self.query_set['word'], self.query_set['label'])]
 
