33c541358ea0890621ec3f5699ff7b60cf1dae51
lisskor <elizaveta.korotkova@gmail.com>
2024-01-10 18:30:47 +0100

    predict takes and returns back an index tuple

--- few_nerd_prompting/clarifai_prompter.py
+++ few_nerd_prompting/clarifai_prompter.py
@@ -5,6 +5,8 @@ from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
 from clarifai_grpc.grpc.api.status import status_code_pb2
 
+from typing import Tuple, Any
+
 
 class ClarifaiPrompter:
     # based on https://github.com/isaac-chung/tweetBot98/blob/main/llm.py
@@ -29,11 +31,11 @@ class ClarifaiPrompter:
             metadata=self.metadata
         )
 
-    def predict(self, model_id, raw_text_ner, retries=3) -> str:
+    def predict(self, model_id, raw_text_ner, index, retries=3) -> Tuple[str, Tuple[Any, ...]]:
         for i in range(retries):
             post_model_outputs_response = self._predict(model_id, [raw_text_ner])
             if post_model_outputs_response.status.code == status_code_pb2.SUCCESS:
-                return post_model_outputs_response.outputs[0].data.text.raw
+                return post_model_outputs_response.outputs[0].data.text.raw, index
             if i == retries - 1:
                 logging.error(post_model_outputs_response.status)
                 raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
