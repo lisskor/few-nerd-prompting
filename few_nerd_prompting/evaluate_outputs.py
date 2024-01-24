bcb39504b939db4e5a6bf4ad9a596c6b9dd0c03c
lisskor <elizaveta.korotkova@gmail.com>
2024-01-24 17:25:32 +0200

    Match inputs & outputs by ID + a few fixes

--- few_nerd_prompting/evaluate_outputs.py
+++ few_nerd_prompting/evaluate_outputs.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import logging
-from typing import List
+from typing import List, Dict
 
 from seqeval.metrics import accuracy_score, precision_score, recall_score, f1_score
 from seqeval.metrics import classification_report
@@ -20,34 +20,39 @@ def single_class(labels: List[str], entity_class: str) -> List[str]:
     return [label if label == entity_class else "O" for label in labels]
 
 
-def get_true_labels(filename: str, entity_class: str, max_episodes: int = None, coarse_grained: bool = True) -> List[List[List[str]]]:
+def get_true_labels(filename: str, entity_class: str, pred_ids: List[int] = None, coarse_grained: bool = True) -> Dict[int, List[List[str]]]:
     all_episodes = FewNerdEpisodesSet(filename)
-    all_true_labels = []
+    all_true_labels = {}
 
-    episode_counter = 0
+    episode_id = 0
     for episode in all_episodes.episodes:
-        if max_episodes and episode_counter >= max_episodes:
-            break
-        episode.gpt_ner_examples_from_episode(entity_class)
-        if coarse_grained:
-            all_true_labels.append([single_class(coarse_grained_from_fine_grained(query), entity_class)
-                                    for query in episode.query_labels])
-        else:
-            all_true_labels.append(episode.query_labels)
-        episode_counter += 1
+        if episode_id in pred_ids:
+            all_true_labels[episode_id] = []
+            episode.gpt_ner_examples_from_episode(entity_class)
+            if coarse_grained:
+                all_true_labels[episode_id].extend([single_class(coarse_grained_from_fine_grained(query), entity_class)
+                                                    for query in episode.query_labels])
+            else:
+                all_true_labels[episode_id].extend(episode.query_labels)
+
+        episode_id += 1
 
     return all_true_labels
 
 
-def read_predicted_labels_single_class(filename: str, entity_class: str) -> List[List[List[str]]]:
-    all_predicted_labels = []
+def read_predicted_labels_single_class(filename: str, entity_class: str) -> Dict[int, List[List[str]]]:
+    all_predicted_labels = {}
     with open(filename, 'r', encoding="utf8") as fh:
         for line in fh.readlines():
-            all_predicted_labels.append(json.loads(line.strip())['label'][entity_class])
+            prediction = json.loads(line.strip())
+            episode_id = list(prediction.keys())[0]
+            all_predicted_labels.setdefault(int(episode_id), prediction[episode_id]['label'][entity_class])
     return all_predicted_labels
 
 
 def report(y_true: List[List[str]], y_pred: List[List[str]]):
+    assert([len(s) for s in y_true] == [len(s) for s in y_pred]), "Token counts do not match"
+
     print(f"ACC: {accuracy_score(y_true, y_pred)}")
     print(f"PREC: {precision_score(y_true, y_pred)}")
     print(f"REC: {recall_score(y_true, y_pred)}")
@@ -58,12 +63,22 @@ def report(y_true: List[List[str]], y_pred: List[List[str]]):
 def main(args):
     for entity_class in args.entity_classes:
         pred_labels = read_predicted_labels_single_class(args.pred_file, entity_class)
-        true_labels = get_true_labels(args.few_nerd_file, entity_class, len(pred_labels))
+        true_labels = get_true_labels(args.few_nerd_file, entity_class, [key for key, value in sorted(pred_labels.items())])
 
         logging.info(f"Class: {entity_class}")
 
-        report([item for sublist in true_labels for item in sublist],
-               [item for sublist in pred_labels for item in sublist])
+        # Transform predicted and true labels from dicts into lists
+        sorted_true_list = [value for key, value in sorted(true_labels.items())]
+        sorted_pred_list = [value for key, value in sorted(pred_labels.items())]
+        true_flattened = [item for sublist in sorted_true_list for item in sublist]
+        pred_flattened = [item for sublist in sorted_pred_list for item in sublist]
+
+        # Temporary fix: fill the labels that errored out with O's
+        for i in range(len(true_flattened)):
+            if not pred_flattened[i]:
+                pred_flattened[i] = ["O"] * len(true_flattened[i])
+
+        report(true_flattened, pred_flattened)
 
 
 if __name__ == '__main__':
